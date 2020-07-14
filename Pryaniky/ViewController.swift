316898f2ee489f84692ab4a63880ee470f4b6bde
makson984 <makson984@mail.ru>
2020-07-14 19:10:34 +0300

    Initial Commit

---
+++ Pryaniky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pryaniky
+//
+//  Created by Максон on 14.07.2020.
+//  Copyright © 2020 MaksonInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
